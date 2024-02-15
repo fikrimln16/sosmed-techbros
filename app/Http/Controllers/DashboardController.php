8188fdc4db94f23bd75daf9db9d4824bc5d9dbf5
Fikri Maulana <maulanafikri455@gmail.com>
2024-02-15 11:07:26 +0700

    add rateLimiter

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
 
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+
 
 class DashboardController extends Controller
 {
@@ -27,21 +29,24 @@ class DashboardController extends Controller
         ]);
     }
 
-    public function store(Request $request){
-        // Menggunakan auth() helper untuk mendapatkan user yang sedang login
-        $user = auth()->user();
-    
-        $validatedData = $request->validate([
-            'body' => 'required|string|max:480'
-        ]);
-        
-        // Create a new post instance
-        $post = new Post();
-        $post->body = $validatedData['body'];
-        $post->user_id = Auth::id();
-        $post->save();
-    
-        return redirect()->route('dashboard')->with('success', 'Post created!');
+    public function store(Request $request)
+    {
+        try {
+            $user = auth()->user();
+
+            $validatedData = $request->validate([
+                'body' => 'required|string|max:480'
+            ]);
+
+            $post = new Post();
+            $post->body = $validatedData['body'];
+            $post->user_id = auth()->id(); // Menggunakan auth()->id() untuk mendapatkan ID user
+            $post->save();
+
+            return redirect()->route('dashboard')->with('success', 'Post created!');
+        } catch (ThrottleRequestsException $e) {
+            return redirect()->route('dashboard')->with('success', 'Post creation rate limit exceeded. Please try again later.');
+        }
     }
 
     public function like($id){
