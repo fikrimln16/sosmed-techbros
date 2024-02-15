207c3883c573a09a8a2e1846a198373ecb680f2b
Fikri Maulana <maulanafikri455@gmail.com>
2024-02-15 09:49:34 +0700

    add error message

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -16,21 +16,33 @@
                        <div class="mb-3">
                            <label for="name" class="form-label">Name</label>
                            <input type="text" id="name" name="name" class="form-control" required autofocus>
+                           @error('name')
+                               <span class="d-block fs-6 text-danger mt-2"> {{ $message }}</span>
+                           @enderror
                        </div>
 
                        <div class="mb-3">
                            <label for="email" class="form-label">Email</label>
                            <input type="email" id="email" name="email" class="form-control" required>
+                           @error('email')
+                               <span class="d-block fs-6 text-danger mt-2"> {{ $message }}</span>
+                           @enderror
                        </div>
 
                        <div class="mb-3">
                            <label for="password" class="form-label">Password</label>
                            <input type="password" id="password" name="password" class="form-control" required>
+                           @error('password')
+                               <span class="d-block fs-6 text-danger mt-2"> {{ $message }}</span>
+                           @enderror
                        </div>
 
                        <div class="mb-3">
                            <label for="password_confirmation" class="form-label">Confirm Password</label>
                            <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" required>
+                           @error('password_confirmation')
+                               <span class="d-block fs-6 text-danger mt-2"> {{ $message }}</span>
+                           @enderror
                        </div>
 
                        <div class="d-grid gap-2">
