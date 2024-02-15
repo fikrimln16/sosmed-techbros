3ee809dc4ab5597ce4b59b314fcea60bea9e4fe5
Fikri Maulana <maulanafikri455@gmail.com>
2024-02-16 05:07:40 +0700

    add href on comment user

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,13 +1,13 @@
 <hr>
 <div class="d-flex align-items-start fs-4">
-      <a href="">
+      <a href="/profile/{{ $comment->user->id }}">
          <img style="width: 35px" class="me-2 avatar-sm rounded-circle border border-black"
             src="{{ $comment->user->avatar }}" alt="Luigi Avatar" />
       </a>
       <div class="w-100">
          <div class="d-flex justify-content-between">
             <div class="d-flex gap-1">
-                  <a href="" class="text-decoration-none">
+                  <a href="/profile/{{ $comment->user->id }}" class="text-decoration-none">
                      <h6 class="mt-2">{{ $comment->user->name }}</h6>
                   </a>
                   <i class="bx bx-message-rounded-dots"></i>
